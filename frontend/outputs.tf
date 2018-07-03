a7ff94a5af95f84abbe91ea2d834881e5ee8d631
jspc <james@zero-internet.org.uk>
2018-07-03 17:21:37 +0100

    Provide more readable code

--- frontend/outputs.tf
+++ frontend/outputs.tf
@@ -1,9 +1,9 @@
 output "fqdn" {
   description = "Fully qualified domain name for this frontend service"
-  value = "${digitalocean_record.loadbalancer.fqdn}"
+  value       = "${digitalocean_record.loadbalancer.fqdn}"
 }
 
 output "lb_ip" {
   description = "IP address of loadbalancer"
-  value = "${digitalocean_loadbalancer.public.ip}"
+  value       = "${digitalocean_loadbalancer.public.ip}"
 }
--- frontend/vars.tf
+++ frontend/vars.tf
@@ -1,27 +1,44 @@
 variable "loadbalancer_tag" {
-  default = "worker"
+  description = "Digital Ocean tag on droplets which will sit behind firewall"
+  default     = "worker"
+  type        = "string"
 }
 
 variable "app_port" {
-  default = 8000
+  description = "Port on droplets which will be fronted with loadbalancer"
+  default     = 8000
 }
 
 variable "subdomain" {
-  default = "weather"
+  description = "Subdomain on which to create DNS records"
+  default     = "weather"
+  type        = "string"
 }
 
 variable "domain" {
-  default = "jspc.pw"
+  description = "Domain on which to create DNS records"
+  default     = "jspc.pw"
+  type        = "string"
 }
 
 variable "tls_private_key" {
-  default = ".secrets/selfsigned/key.pem"
+  description = "Private Key for TLS certs"
+  default     = ".secrets/selfsigned/key.pem"
+  type        = "string"
 }
 
 variable "tls_cert" {
-  default = ".secrets/selfsigned/certificate.pem"
+  description = "TLS Certificate"
+  default     = ".secrets/selfsigned/certificate.pem"
+  type        = "string"
 }
 
+// Originally I aimed to use a ternary here to determine whether the variable was empty as per:
+//    "${var.tls_chain == "" ? "" : file(var.tls_chain)}"
+// This wont work because terraform evaluates both sides of the ternary before choosing one.
+// See: https://github.com/hashicorp/hil/issues/50
 variable "tls_chain" {
-  default = ".secrets/selfsigned/chain"
+  description = "TLS Trust Chain. This must be set, though may be an empty file"
+  default     = ".secrets/selfsigned/chain"
+  type        = "string"
 }
