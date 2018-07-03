370f97d2357942ecdb4cc2e62c9541c7468d81e3
jspc <james@zero-internet.org.uk>
2018-07-03 12:46:38 +0100

    Wrap lb creation in frontend module

--- healthcheck.tf
+++ healthcheck.tf
@@ -1,39 +1,13 @@
-resource "digitalocean_certificate" "loadbalancer" {
-  name              = "jspc.pw"
-  private_key       = "${file(var.tls_private_key)}"
-  leaf_certificate  = "${file(var.tls_cert)}"
-  certificate_chain = "${file(var.tls_chain)}"
-}
-
-# Create a new Load Balancer with TLS termination
-resource "digitalocean_loadbalancer" "healthcheck-public" {
-  name                   = "healthcheck-public"
-  region                 = "lon1"
-  droplet_tag            = "${digitalocean_tag.worker.name}"
-  redirect_http_to_https = true
-
-  forwarding_rule {
-    entry_port     = 443
-    entry_protocol = "https"
-
-    target_port     = "${var.healthcheck_port}"
-    target_protocol = "http"
-
-    certificate_id = "${digitalocean_certificate.loadbalancer.id}"
-  }
-
-  healthcheck {
-    port     = 8080
-    protocol = "tcp"
-  }
-}
-
-resource "digitalocean_record" "loadbalancer" {
-  domain = "jspc.pw"
-  type   = "A"
-  name   = "lb"
-  ttl    = 30
-  value  = "${digitalocean_loadbalancer.healthcheck-public.ip}"
+module "frontend" {
+  name             = "healthcheck"
+  source           = "./frontend"
+  loadbalancer_tag = "${digitalocean_tag.worker.name}"
+  app_port         = "${var.healthcheck_port}"
+  subdomain        = "lb"
+  domain           = "jspc.pw"
+  tls_private_key  = "${var.healthcheck_tls_private_key}"
+  tls_cert         = "${var.healthcheck_tls_cert}"
+  tls_chain        = "${var.healthcheck_tls_chain}"
 }
 
 resource "kubernetes_service" "healthcheck" {
@@ -49,8 +23,8 @@ resource "kubernetes_service" "healthcheck" {
     session_affinity = "ClientIP"
 
     port {
-      port        = 8080
-      target_port = 8080
+      port        = "${var.healthcheck_port}"
+      target_port = "${var.healthcheck_port}"
     }
 
     type         = "NodePort"
---
+++ weather-api.tf
@@ -0,0 +1,10 @@
+module "weather-frontend" {
+  source           = "./frontend"
+  loadbalancer_tag = "${digitalocean_tag.worker.name}"
+  app_port         = "${var.healthcheck_port}"
+  subdomain        = "weather"
+  domain           = "jspc.pw"
+  tls_private_key  = "${var.weather_tls_private_key}"
+  tls_cert         = "${var.weather_tls_cert}"
+  tls_chain        = "${var.weather_tls_chain}"
+}
